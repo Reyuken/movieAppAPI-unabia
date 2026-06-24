e4e3a287919e600dff224e40f923edef80cc3264
Ray Unabia <unabiaray2@gmail.com>
2026-06-24 18:59:28 +0800

    Remove verification middleware from 'get all movies' route

--- routes/movies.js
+++ routes/movies.js
@@ -5,7 +5,7 @@ const movieController = require("../controllers/movies");
 const { verify, verifyAdmin } = require("../auth");
 
 router.post("/", verify, verifyAdmin, movieController.addMovie);
-router.get("/all", verify, movieController.getAllMovies);
+router.get("/all", movieController.getAllMovies);
 router.get("/:movieId", verify, movieController.getMovie);
 router.patch("/:movieId/update", verify, verifyAdmin, movieController.updateMovie);
 router.delete("/:movieId/delete", verify, verifyAdmin, movieController.deleteMovie);
